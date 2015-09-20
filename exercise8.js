e32739dbbf28dc39fc5689696870ff2376405c91
sez <sesha911@gmail.com>
2015-09-19 18:04:18 -0700

    Sesha: Exercise 8 solved

---
+++ exercise8.js
@@ -0,0 +1,29 @@
+var concatStream = require('concat-stream');
+var http = require('http');
+var url = process.argv[2];
+
+var collectedStream = function(data) {
+	console.log(data.length);
+	console.log(data);
+};
+
+http.get(url, function(response) {
+	response.setEncoding("utf8");
+	response.on("error", console.error);
+	response.pipe(concatStream(collectedStream));
+});
+
+/* ALTERNATIVE
+
+	var http = require('http');
+    var bl = require('bl');
+    
+    http.get(process.argv[2], function (response) {
+      	response.pipe(bl(function (err, data) {
+        	if (err) return console.error(err)
+        	data = data.toString()
+        	console.log(data.length)
+        	console.log(data)
+		}));
+	});
+*/
